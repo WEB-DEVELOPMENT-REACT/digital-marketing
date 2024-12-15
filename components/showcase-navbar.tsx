d8fed09f1e08f38d2cc573fe7fd2818df65ddd05
Duverney muriel <duverwild@gmail.com>
2024-12-15 09:11:36 +0100

    number out

--- components/showcase-navbar.tsx
+++ components/showcase-navbar.tsx
@@ -65,12 +65,7 @@ xl:w-4/5
 
 
       <div className="flex items-center gap-x-4">
-      <a href="tel:5193191562" className="hidden xl:flex">
-              <button className="px-4 py-2  rounded-md flex items-center gap-x-3 ">
-            
-            (673)-685-542
-              </button>
-            </a>
+   
 
 
       <Link
