f2cfc55f4a5577c56c38eb794395e9983442dd41
Duverney muriel <duverwild@gmail.com>
2024-12-17 16:34:19 +0100

    css modified

--- components/ui/lets-make-things-happen.tsx
+++ components/ui/lets-make-things-happen.tsx
@@ -10,7 +10,7 @@ function LetsMakeThingsHappenSection() {
     bg-accent rounded-[45px] p-[50px] md:p-[60px] relative">
       <div className="md:pr-[22rem]">
         <p className="text-3xl font-medium">
-          Hagamos Esto Una Realidad
+          Contacte con un agente hoy
         </p>
 
         <p className="my-10 text-xl">
@@ -18,26 +18,49 @@ function LetsMakeThingsHappenSection() {
         pueden ayudar a que su negocio crezca y tenga éxito en línea.
         </p>
 
-        <Link
-          href="/meeting"
-          className="py-3 
-            px-10
-        
-            md:px-16
-      md:text-xl
-      hover:bg-[#abcbff] 
-      rounded-[6px]
-      border-2 
-      border-black 
-      dark:border-white 
-       bg-[#121212] 
-       text-white 
-       transition 
-       duration-200 
-       hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
-        >
-         Agendar Cita
-        </Link>
+        <div
+    className="
+         flex 
+          md:justify-center 
+          items-center 
+          gap-x-4
+           "
+  >
+    <Link
+      href="/meeting"
+      className="py-3 
+    px-10
+    md:px-16
+md:text-xl
+hover:bg-[#abcbff] 
+rounded-[6px]
+border-2 
+border-black 
+dark:border-white 
+bg-[#121212] 
+text-white 
+transition 
+duration-200 
+hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+    >
+     Agendar cita
+    </Link>
+    <Link
+      href={"/pricing"}
+      className="
+      bg-white
+py-3 
+px-10
+md:px-16
+md:text-xl
+border-4
+border-black
+rounded-[6px]
+hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+    >
+      Precios
+    </Link>
+  </div>
       </div>
       <div className="absolute -top-8 right-8 hidden md:block">
         <Image
