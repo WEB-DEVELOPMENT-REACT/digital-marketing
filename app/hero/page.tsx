dd0eef6c8ce69bd8b4f3f5121a74c86fedf600fa
Duverney muriel <duverwild@gmail.com>
2025-01-12 15:57:09 +0100

    Videos Section added

--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
            "
   >
     <Link
-      href="/meeting"
+      href="/videos"
       className="py-3 
     px-10
     md:px-16
@@ -52,7 +52,7 @@ transition
 duration-200 
 hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
     >
-     Agendar
+     Proceso
     </Link>
     <Link
       href={"/showcase"}
